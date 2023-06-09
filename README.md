20e061944dfa2ed2e217786ee93c76b85558c9bf
hviiet <nviet8640@gmail.com>
2023-06-09 21:52:41 +0700

    add vallidate

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PBL3-BloodDono
+
 Dự án môn học PBL3 - Công nghệ phần mềm
+Front End: Nguyễn Hữu Thành
+Back End: Nguyễn Hoàng Việt
+Database: Trần Thanh Tùng
--- index.js
+++ index.js
@@ -18,26 +18,24 @@ app.use(express.json());
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 //set static folder
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, './public')));
 //Auth Page
-app.use('/', require('../routes/auth'));
+app.use('/', require('./routes/auth'));
 //Home Page
-app.use('/', require('../routes/home'));
+app.use('/', require('./routes/home'));
 //Profile Page
-app.use('/user', require('../routes/user'));
+app.use('/user', require('./routes/user'));
 //Event Page
-app.use('/event', require('../routes/event'));
+app.use('/event', require('./routes/event'));
 //Search 
-app.use('/search', require('../routes/search'));
+app.use('/search', require('./routes/search'));
 //All api
-app.use('/api', require('../routes/api'))
+app.use('/api', require('./routes/api'))
 // Error 404
 app.use('/404', (req, res) => {
     res.status(404).render('error404');
 });
 
-//test route - testing only
-app.use('/', require('../routes/test'));
 
 
 app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`));
